b5d64d9223341e2848c85aa6f2972c219fbff329
03681067058 <03681067058@LAB205-10.fw.iffar.edu.br>
2022-10-06 16:50:45 -0600

    WEEK 2 - CLASS CONNECTION

---
+++ Estacionamento/src/connection/script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS estacionamento;
+USE estacionamento;
+
+CREATE TABLE vaga (
+    idVaga int NOT NULL AUTO_INCREMENT,
+    numero int NOT NULL,
+    rua    varchar(100) NOT NULL,
+    obliqua boolean NOT NULL,
+    PRIMARY KEY (idVaga));
+
+
