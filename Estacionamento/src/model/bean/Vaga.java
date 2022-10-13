fa5850cd21e541a08066fe3d5466c50ce3a9ff37
03681067058 <03681067058@LAB205-10.fw.iffar.edu.br>
2022-10-13 16:51:54 -0600

    semana 3 estacionamento AAAAAAAAAAAAAAAAAAAAAA

---
+++ Estacionamento/src/model/bean/Vaga.java
@@ -0,0 +1,44 @@
+package model.bean;
+
+
+public class Vaga {
+    
+    private int idVaga;
+    private int numero;
+    private String rua;
+    private boolean obliqua;
+
+    public int getIdVaga() {
+        return idVaga;
+    }
+
+    public void setIdVaga(int idVaga) {
+        this.idVaga = idVaga;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public boolean isObliqua() {
+        return obliqua;
+    }
+
+    public void setObliquo(boolean obliqua) {
+        this.obliqua = obliqua;
+    }
+    
+    
+}
